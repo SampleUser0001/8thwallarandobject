80be38b1e1f89c3885ea4d6383e1c03ddb39c0f1
Satoru Tanaka <ittimfn@gmail.com>
2020-12-04 15:42:33 +0900

    empty app.js

--- app.js
+++ app.js
@@ -3,6 +3,3 @@
 // app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
 // is loaded, and before body.html is loaded.
 
-import './bottom-circle.css';
-import {bottomCircleDisplay} from './bottom-circle';
-AFRAME.registerComponent('bottom-circle', bottomCircleDisplay());
