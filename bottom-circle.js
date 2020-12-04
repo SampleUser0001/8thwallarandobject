dea3817893400d95ac36cd6b089f6a5f0648d80c
Satoru Tanaka <ittimfn@gmail.com>
2020-12-04 15:37:08 +0900

    test

---
+++ bottom-circle.js
@@ -0,0 +1,8 @@
+const bottomCircleDisplay = () => ({
+    init() {
+      const nextButton = document.getElementById('bottomCircleButton');
+      nextButton.style.display = 'block';
+    },
+  })
+  
+  export {bottomCircleDisplay}
