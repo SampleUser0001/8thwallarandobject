98fdaf3b1e05a87c57ecc2a78955adc1915e0cb1
Satoru Tanaka <ittimfn@gmail.com>
2020-12-15 14:59:27 +0900

    S3にUP

---
+++ README.md
@@ -0,0 +1,7 @@
+# 8thwall_ar_and_object
+
+## URL
+
+### S3
+
+[https://ittimfn-public.s3-ap-northeast-1.amazonaws.com/AR/googleanalytics_cloudfront/index.html](https://ittimfn-public.s3-ap-northeast-1.amazonaws.com/AR/googleanalytics_cloudfront/index.html)
---
+++ release_toS3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export S3_DIR=s3://ittimfn-public/AR/googleanalytics_cloudfront
+aws s3 rm ${S3_DIR} --recursive
+
+aws s3 cp app.js             ${S3_DIR}/
+aws s3 cp bottom-circle.css  ${S3_DIR}/
+aws s3 cp bottom-circle.js   ${S3_DIR}/
+aws s3 cp index.html         ${S3_DIR}/
+
+aws s3 cp asserts            ${S3_DIR}/asserts --recursive
